8b7126012e820878de3b1eb3c0bdb6597549c624
andrea-salvalaggio <andrea.salvalaggio95@gmail.com>
2022-08-23 17:26:13 +0200

    inzio snack-5

---
+++ snack-5/index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP Snacks Blocco 1</title>
+</head>
+<body>
+
+    <section>
+
+        <h2>Snack 5</h2>
+        <hr>
+        <i> Creare un array contenente qualche alunno di un'ipotetica classe. 
+            Ogni alunno avrà Nome, Cognome e un array contenente i suoi voti scolastici. 
+            Stampare Nome, Cognome e la media dei voti di ogni alunno.
+        </i>
+        <hr>
+
+        <?php 
+            $classes = [
+                [
+                    "first_name" => "LA Lakers",
+                    "last_name" => "GS Warrios",
+                ],
+            ];
+        ?>
+
+    </section>
+     
+</body>
+</html>
